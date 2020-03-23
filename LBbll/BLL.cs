b6ec64f09636086940317d843c3fe7c8c8bce105
pblawler <pblawler@gmail.com>
2020-03-22 23:59:15 -0500

    Fixed namespace error

---
+++ LBbll/BLL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LBbll
+{
+
+    public class BLL
+    {
+
+    }
+}
+
--- LawlerBallisticsDesk/Classes/Guns/GunMsg.cs
+++ LawlerBallisticsDesk/Classes/Guns/GunMsg.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LawlerBallisticsDesk.Classes.Gun
+namespace LawlerBallisticsDesk.Classes.Guns
 {
     class GunBarrelMsg
     {
