600ae8bcc01e9c288b09457cc83a252221b190dc
pblawler <pblawler@gmail.com>
2026-03-12 11:58:38 -0500

    * 'Line 2: Replaced the obsolete 'GalaSoft.MvvmLight.Command' namespace with 'CommunityToolkit.Mvvm.Input' as 'RelayCommand' is now provided by CommunityToolkit.Mvvm.Input.' in file 'LawlerBallisticsDesk\ViewModel\RecipeViewModel.cs'

--- LawlerBallisticsDesk/ViewModel/RecipeViewModel.cs
+++ LawlerBallisticsDesk/ViewModel/RecipeViewModel.cs
@@ -1,5 +1,5 @@
 ﻿using GalaSoft.MvvmLight;
-using GalaSoft.MvvmLight.Command;
+using CommunityToolkit.Mvvm.Input;
 using GalaSoft.MvvmLight.Messaging;
 using LawlerBallisticsDesk.Classes;
 using LawlerBallisticsDesk.Views.Cartridges;
